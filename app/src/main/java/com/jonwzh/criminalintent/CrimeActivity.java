782d5335ab724349302da41fc8929164c98f030b
Zihao Wong <Jonathan@ZihaodeMacBook-Pro.local>
2018-08-12 19:17:27 -0400

    #10.1: Add the functionality to click the items in CrimeList and update the information for the crimes

--- app/src/main/java/com/jonwzh/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/jonwzh/criminalintent/CrimeActivity.java
@@ -1,14 +1,15 @@
 package com.jonwzh.criminalintent;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.os.Bundle;
+import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID)getIntent()
+                .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 }
